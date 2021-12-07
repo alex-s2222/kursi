2d2cadf032211f02a4f98a22aceb2aee539ebb69
Admin <alexs22221@gmail.com>
2021-12-07 22:52:22 +0300

    рисуем круги

--- dz2/00_bubbles.py
+++ dz2/00_bubbles.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 
-sd.resolution = (1200, 600)
-
-# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
-
-# Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
-
-# Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+point = sd.get_point(300, 300)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+for y in range(100, 301, 100):
+    for x in range(100, 1100, 100):
+        point = sd.get_point(x, y)
+        #bubble(point=point, step=1)
+
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
+for _ in range(100):
+    point = sd.random_point()
+    step = random.randint(2, 10)
+    bubble(point=point, step=step)
+
 
 sd.pause()
-
-
