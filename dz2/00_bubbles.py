923172f543050887a4869c6eb54a10220a873e3f
Admin <alexs22221@gmail.com>
2021-12-12 12:25:33 +0300

    circle are drawn

--- dz2/00_bubbles.py
+++ dz2/00_bubbles.py
@@ -11,10 +11,15 @@ for y in range(100, 301, 100):
         point = sd.get_point(x, y)
         #bubble(point=point, step=1)
 
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius, width=2)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
-for _ in range(100):
+for _ in range(5):
     point = sd.random_point()
     step = random.randint(2, 10)
     bubble(point=point, step=step)
