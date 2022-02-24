07232c1997f58e441272edc9983335e32c8b12c7
alex-s <alexs22221@gmail.com>
2022-02-24 23:32:56 +0300

    complete

--- dz7/08_practice.py
+++ dz7/08_practice.py
@@ -1,81 +1,69 @@
 # -*- coding: utf-8 -*-
-
 from random import randint
 
 
-# Реализуем модель человека.
-# Человек может есть, работать, играть, ходить в магазин.
-# У человека есть степень сытости, немного еды и денег.
-# Если сытость < 0 единиц, человек умирает.
-# Человеку надо прожить 365 дней.
-from termcolor import cprint
-
-
 class Man:
-
     def __init__(self, name):
         self.name = name
         self.fullness = 50
         self.house = None
 
     def __str__(self):
-        return 'Я - {}, сытость {}'.format(
+        return 'Я-{}, сытость {}'.format(
             self.name, self.fullness)
 
     def eat(self):
         if self.house.food >= 10:
-            cprint('{} поел'.format(self.name), color='yellow')
+            print('{} поел'.format(self.name))
             self.fullness += 10
             self.house.food -= 10
         else:
-            cprint('{} нет еды'.format(self.name), color='red')
+            print('{} нет еды'.format(self.name))
 
     def work(self):
-        cprint('{} сходил на работу'.format(self.name), color='blue')
-        self.house.money += 50
+        print('{} сходил на работу'.format(self.name))
+        self.house.money += 30
         self.fullness -= 10
 
-    def watch_MTV(self):
-        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+    def watch_NTV(self):
+        print('{} смотрел НТВ целый день'.format(self.name))
         self.fullness -= 10
 
     def shopping(self):
-        if self.house.money >= 50:
-            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
-            self.house.money -= 50
-            self.house.food += 50
+        if self.house.money >= 30:
+            print('{} сходил в магазин'.format(self.name))
+            self.house.money -= 30
+            self.house.food += 30
         else:
-            cprint('{} деньги кончились!'.format(self.name), color='red')
+            print('{} кончились деньги!'.format(self.name))
 
-    def go_to_the_house(self, house):
+    def go_into_to_house(self, house):
         self.house = house
-        self.fullness -= 10
-        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+        print('{} вьехал в дом'.format(self.name))
 
     def act(self):
         if self.fullness <= 0:
-            cprint('{} умер...'.format(self.name), color='red')
-            return
+            print('{} die'.format(self.name))
+            return 1
         dice = randint(1, 6)
         if self.fullness < 20:
             self.eat()
-        elif self.house.food < 10:
+        elif self.house.food <= 30:
             self.shopping()
-        elif self.house.money < 50:
+        elif self.house.money <= 30:
             self.work()
         elif dice == 1:
             self.work()
         elif dice == 2:
             self.eat()
         else:
-            self.watch_MTV()
+            self.watch_NTV()
 
 
 class House:
-
     def __init__(self):
-        self.food = 50
-        self.money = 0
+        self.food = 10
+        self.money = 50
 
     def __str__(self):
         return 'В доме еды осталось {}, денег осталось {}'.format(
@@ -85,22 +73,26 @@ class House:
 citizens = [
     Man(name='Бивис'),
     Man(name='Батхед'),
-    Man(name='Кенни'),
+    Man(name='Барт')
 ]
 
-
 my_sweet_home = House()
-for citisen in citizens:
-    citisen.go_to_the_house(house=my_sweet_home)
+for citizen in citizens:
+    citizen.go_into_to_house(house=my_sweet_home)
 
-for day in range(1, 366):
-    print('================ день {} =================='.format(day))
-    for citisen in citizens:
-        citisen.act()
-    print('--- в конце дня ---')
-    for citisen in citizens:
-        print(citisen)
+for day in range(1, 321):
+    print('\n================= день {} ============='.format(day))
+    for citizen in citizens:
+        die = citizen.act()
+        if die == 1:
+            break
+    if die == 1:
+        break
+    print('================================================')
+    for citizen in citizens:
+        print(citizen)
     print(my_sweet_home)
+
 
 # Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
 # Нужен класс Дом, в нем должн быть холодильник с едой и тумбочка с деньгами
