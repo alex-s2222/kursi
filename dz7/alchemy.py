5dfd96a730c00716859e16ac8a244e4d82a38c41
alex-s <alexs22221@gmail.com>
2022-02-28 17:19:34 +0300

    complete

--- dz7/alchemy.py
+++ dz7/alchemy.py
@@ -14,12 +14,90 @@
 # Сложение элементов реализовывать через __add__
 # Если результат не определен - то возвращать None
 # Вывод элемента на консоль реализовывать через __str__
-#
+
 # Примеры преобразований:
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Fire:
+    def __init__(self):
+        self.name = 'Огонь'
+
+    def __add__(self, other):
+        if other.name == 'Земля':
+            return 'Лава'
+        elif other.name == 'Воздух':
+            return 'Молния'
+        elif other.name == 'Вода':
+            return 'Пар'
+        else:
+            return None
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Earth:
+    def __init__(self):
+        self.name = 'Земля'
+
+    def __add__(self, other):
+        if other.name == 'Огонь':
+            return 'Лава'
+        elif other.name == 'Воздух':
+            return 'Пыль'
+        elif other.name == 'Вода':
+            return 'Грязь'
+        else:
+            return None
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Air:
+    def __init__(self):
+        self.name = 'Воздух'
+
+    def __add__(self, other):
+        if other.name == 'Земля':
+            return 'Пыль'
+        elif other.name == 'Воздух':
+            return 'Ураган'
+        elif other.name == 'Вода':
+            return 'Шторм'
+        elif other.name == 'Огонь':
+            return 'Молния'
+        else:
+            return None
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Water:
+    def __init__(self):
+        self.name = 'Вода'
+
+    def __add__(self, other):
+        if other.name == 'Земля':
+            return 'Грязь'
+        elif other.name == 'Воздух':
+            return 'Шторм'
+        else:
+            return None
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+print(Fire(), ' + ', Earth(), '=', Fire()+Earth())
+print(Fire(), ' + ', Water(), '=', Fire()+Water())
+print(Fire(), ' + ', Air(), '=', Fire()+Air())
+print(f'{Water()} + {Water()} = {Water()+Water()}')
+print(f'{Earth()}  + {Water()} = {Earth()+Water()}')
+print(f'{Air()} + {Earth()} = {Earth()+Air()}')
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
